f8510e1048793c600458e8b31e82d661b630a91f
ItsRubenX <ruben.harmsen@emtr.nl>
2025-09-24 14:27:40 +0200

    RegisterView bestanden toegevoegd

---
+++ Grocery.App/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+
+namespace Grocery.App.ViewModels
+{
+    public partial class RegisterViewModel : BaseViewModel
+    {
+        
+    }
+}
